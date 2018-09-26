a55b923221f12f6a627dc357b8cde00f87b9cbd5
VinSia <VinSia@live.cn>
2018-09-26 23:16:19 +0800

    fix gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function (grunt) {
+    var config = require('./.screeps.json')
+    grunt.loadNpmTasks('grunt-screeps');
+
+    grunt.initConfig({
+        screeps: {
+            options: {
+                email: config.email,
+                password: config.password,
+                branch: config.branch,
+                ptr: config.ptr
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'dist/',
+                    src: ['src/*.{js,wasm}'],
+                    flatten: true
+                }]
+            }
+        }
+    });
+}
